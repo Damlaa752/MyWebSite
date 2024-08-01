67ceccafb946b9fbd7d9f56e12853c68f7494d08
Damlaa752 <gultekindamla7@gmail.com>
2024-08-01 09:49:32 +0300

    Add project files.

---
+++ Domain/Identity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Identity
+{
+    public class User : BaseEntity
+    {
+        [StringLength(50)]
+        public string Username { get; set; }
+        [StringLength(255)]
+        public string PasswordHash { get; set; }
+        [StringLength(50)]
+        public string Role { get; set; }
+    }
+}
